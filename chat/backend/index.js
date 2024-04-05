0c2933b015f3e67ed49f3b60c8d2699a99c3bb04
Sushruthvi <137644925+Sushruthvi@users.noreply.github.com>
2024-04-05 18:04:18 +0530

    :dead: commit-18:04

---
+++ chat/backend/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+  try{
+const r=await axios.put(
+    'https://api.chatengine.io/users/'
+ ,  {username:username,secret:"username",first_name:"username"},
+    {headers:{ "private-key":"d220a5df-a48a-41c4-9932-6efed43009df"}}
+);
+return res.status(r.status).json(r.data)
+  }
+
+  catch(e){
+   return res.status(e.response.status).json(e.response.data)
+  }
+  return res.json({ username: username, secret: "sha256..." });
+});
+
+app.listen(3000);
